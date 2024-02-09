8f1597965a8ebdd927d29ba83eb168abd52edeaf
rjshk013 <rjshk013@gmail.com>
2024-02-09 21:36:28 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+    description = "EC2 Region for the VPC"
+    default = "us-east-1"
+}
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default = "ami-0817d428a6fb68645"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default = "t2.micro"
+}
+variable "key_name" {
+  description = "Public key path"
+  default     = "testebs"
+}
