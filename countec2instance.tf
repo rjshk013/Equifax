ee55fafd667db318bff9fbf5df4fe2f5e7c415c9
rjshk013 <rjshk013@gmail.com>
2024-02-09 22:05:14 +0530

    Create countec2instance.tf

---
+++ countec2instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "example" {
+  count                  = var.instance_count # need to create varible named "instance_count" in variables.tf
+  ami                    = "your-ami-id"
+  instance_type          = "t2.micro"
+  subnet_id              = "your-subnet-id" # Subnet ID within your existing VPC
+  vpc_security_group_ids = ["your-security-group-id"] # Security Group ID within your existing VPC
+
+  tags = {
+    Name = "ExampleInstance-${count.index + 1}"
+  }
+}
