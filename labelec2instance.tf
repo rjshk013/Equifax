588bda96a57ba1ac3efa51e7a007579e245f6dc2
rjshk013 <rjshk013@gmail.com>
2024-02-09 22:21:59 +0530

    Create labelec2instance.tf

---
+++ labelec2instance.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "example" {
+  count                  = var.instance_count
+  ami                    = "your-ami-id"
+  instance_type          = "t2.micro"
+  subnet_id              = "your-subnet-id" # Subnet ID within your existing VPC
+  vpc_security_group_ids = ["your-security-group-id"] # Security Group ID within your existing VPC
+
+  tags = {
+    "Name" = "ExampleInstance-${count.index + 1}"
+    "Commonlabel" = "equifax"  # Common label for all instances can give key like organisation & value as equifax
+    "DynamicLabel" = "Instance${count.index + 1}Label"  # Dynamic label for each instance with count
+  }
+}
